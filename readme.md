05e72fa263f854063cb5702f8b02e72b32091b86
nickkauf <nkauf87@gmail.com>
2021-07-07 11:52:49 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+Assignment:
+
+Robert is working on his 5-year goal : To increase his current $62,000 annual income by 10% each year.
+
+Pseudocode:
+
+set year to 1
+set income to 62000
+increment year by 1
+set income to income*1.1
+increment year by 1
+set income to income*1.1
+increment year by 1
+set income to income*1.1
+increment year by 1
+print "year: " year "income" income
---
+++ src/AmendingVariables.java
@@ -0,0 +1,19 @@
+public class AmendingVariables {
+    public static void main(String[] args)
+    {
+       int year = 1;
+       double income = 62000;
+       year++;
+       income = income*1.1;
+       year++;
+       income = income*1.1;
+       year++;
+       income = income*1.1;
+       year++;
+       income = income*1.1;
+       System.out.println("Year " + year + " income: " + income);
+
+
+
+    }
+}
